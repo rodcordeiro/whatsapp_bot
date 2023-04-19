04afdbf2103d8b3ff6639b78530bd6ab7da9b9e3
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-04-19 14:20:34 -0300

    Chore: added help argument on register

--- src/commands/help/help.command.js
+++ src/commands/help/help.command.js
@@ -2,11 +2,13 @@ const { commandsHandler } = require('../../common/handlers/commands.handler');
 class Command {
   name = 'help';
   execute = async message => {
+    const chat = await message.getChat();
+    chat.sendStateTyping();
+
     const text = commandsHandler.commandsList
       .map(command => `/${command.name}`)
       .join('\n');
     await message.reply(`Here are some commands:\n\n${text}`);
-
     return;
   };
 }
--- src/commands/register/register.command.js
+++ src/commands/register/register.command.js
@@ -1,12 +1,20 @@
 class Command {
   name = 'register';
-  
-  execute = async (message, args) => {
-    // const chat = await message.getChat();
-    // chat.sendStateTyping();
 
+  execute = async (message, args) => {
+    const chat = await message.getChat();
+    chat.sendStateTyping();
+
+    if (args.find(arg => arg == '--help'))
+      return await message.reply(
+        '```/register <key> <value>```\n\nKey options:\n*pontos*: Grupo de pontos;\n*raizes*: Grupo do raizes;',
+      );
+    if (!args.length)
+      return await message.reply(
+        'Correct syntax:\n```/register <key> <value>```',
+      );
     console.log('register called');
-    await message.reply('Registering' + args);
+    await message.reply('Registering: ' + args);
 
     return;
   };
