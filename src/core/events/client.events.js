a4eb7be4be9d98769b96c9239bb349039a1355f2
rodcordeiro <rodrigomendoncca@gmail.com>
2023-04-22 14:20:23 +0000

    Chore: Importing jobs

--- src/core/events/client.events.js
+++ src/core/events/client.events.js
@@ -1,9 +1,6 @@
 const { client } = require('../wweb/client.wweb');
 const { sendQrCode } = require('../../common/utils/qrToDisc.util');
-const { PontosService } = require('../../common/utils/pontos.util');
-const {
-  automatedMessage,
-} = require('../../common/utils/automatedMessage.util');
+
 
 client.on('qr', async qr => {
   await sendQrCode(qr);
@@ -11,6 +8,6 @@ client.on('qr', async qr => {
 
 client.on('ready', async () => {
   client.sendMessage('5511982645275@c.us', "I'm alive");
-  
+  require("../jobs")
   // automatedMessage('5511983808494@c.us', template, 1 * 60 * 1000);
 });
