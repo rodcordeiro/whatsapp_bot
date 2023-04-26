7c9c666c5548795cf14b72eedd88aa338f82db0c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-04-26 18:20:07 -0300

    Feat: sending audio as reply

--- src/common/handlers/music.handler.js
+++ src/common/handlers/music.handler.js
@@ -1,41 +1,41 @@
 class Point {
   constructor(arr) {
-    this.data = arr.map(item=>({
+    this.data = arr.map(item => ({
       id: item.id,
-    ritmo:item.ritmo,
-    idLinha :item.idLinha,
-    linha:item.linha,
-    tipo:item.tipo,
-    ponto:item.ponto,
-    audioLink :item.audioLink,
-    hasAudio  :item.audioLink ? true : false
-    }))    
+      ritmo: item.ritmo,
+      idLinha: item.idLinha,
+      linha: item.linha,
+      tipo: item.tipo,
+      ponto: item.ponto,
+      audioLink: item.audio_link,
+      hasAudio: item.audio_link ? true : false,
+    }));
   }
   getRandom() {
     const random = Math.floor(Math.random() * this.data.length);
     const randomItem = this.data[random];
     return randomItem;
   }
-  filterByLineId(id){
-    return this.data.filter(item=>item.idLinha===id)
+  filterByLineId(id) {
+    return this.data.filter(item => item.idLinha === id);
   }
 }
 
 class Category {
   constructor(arr) {
-    this.data=arr.map(item=>({
-      id:item.id,
-      linha:item.linha,
-      categoria:item.categoria
-    }))
+    this.data = arr.map(item => ({
+      id: item.id,
+      linha: item.linha,
+      categoria: item.categoria,
+    }));
   }
   getRandom() {
     const random = Math.floor(Math.random() * this.data.length);
     const randomItem = this.data[random];
     return randomItem;
   }
-  filterByCategory(id){
-    return this.data.filter(item=>item.idLinha===id)
+  filterByCategory(id) {
+    return this.data.filter(item => item.idLinha === id);
   }
 }
 
--- src/core/jobs/dailyMessage.job.js
+++ src/core/jobs/dailyMessage.job.js
@@ -1,14 +1,16 @@
+const { MessageMedia } = require('whatsapp-web.js');
 const { client } = require('../wweb/client.wweb');
 const { connection } = require('../../database');
 const { PontosService } = require('../../common/utils/pontos.util');
 const { Templates } = require('../../common/handlers/templates.handler');
 
-const DAY_IN_MS = 24 * 60 * 60 * 1000;
+const DAY_IN_MS = 1 * 1 * 5 * 1000;
 
 const sendDailyPonto = async () => {
   const Pontos = new PontosService();
   const { pontos } = await Pontos.getData();
   const data = pontos.getRandom();
+  console.log(data);
   const message = Templates.DAILY(data);
   const { channel_id: destiny } = await connection('bot_tb_registered_channels')
     .select('channel_id')
@@ -16,7 +18,18 @@ const sendDailyPonto = async () => {
     .first();
   console.log({ destiny });
   if (destiny) {
-    await client.sendMessage(destiny, message);
+    if (data.hasAudio) {
+      const audio = await MessageMedia.fromUrl(
+        'https://rodcordeiro.github.io/shares/audio/marcha_imperial.mp3 ',
+      );
+      console.log('audio', audio);
+      const msg = await client.sendMessage(destiny, message);
+      await msg.reply('', undefined, {
+        media: audio,
+      });
+    } else {
+      await client.sendMessage(destiny, message);
+    }
   }
 };
 
