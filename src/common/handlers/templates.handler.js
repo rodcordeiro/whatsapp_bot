13638bc78b7ce0112f4596146d1564badb21d8c1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-04-26 23:20:49 -0300

    Feat: Setup cronjob for daily jobs at 9am

--- src/common/handlers/templates.handler.js
+++ src/common/handlers/templates.handler.js
@@ -1,7 +1,7 @@
-class Templates{
-  static DAILY(data){
-    return `Ponto do dia!!\n\n*Linha:* \`\`\`${data.linha}\`\`\`\n\n*Ritmo:* \`\`\`${data.ritmo}\`\`\`\n\n${data.ponto}\n\n\`\`\`[Mensagem automática]\`\`\``
+class Templates {
+  static DAILY(data) {
+    return `*Ponto do dia!!*\n\n*Linha:* \`\`\`${data.linha}\`\`\`\n\n*Ritmo:* \`\`\`${data.ritmo}\`\`\`\n\n${data.ponto}\n\n\`\`\`~~[Mensagem automática]~~\nPara ver mais pontos acesse:\nhttps://raizes.rodrigocordeiro.com.br/ ou me mande /help\`\`\``;
   }
 }
 
-module.exports.Templates=Templates;
+module.exports.Templates = Templates;
--- src/core/jobs/dailyMessage.job.js
+++ src/core/jobs/dailyMessage.job.js
@@ -1,16 +1,16 @@
 const { MessageMedia } = require('whatsapp-web.js');
+const { schedule } = require('node-cron');
 const { client } = require('../wweb/client.wweb');
 const { connection } = require('../../database');
 const { PontosService } = require('../../common/utils/pontos.util');
 const { Templates } = require('../../common/handlers/templates.handler');
 
-const DAY_IN_MS = 1 * 1 * 5 * 1000;
+const DAILY_CRON = '* 9 * * *';
 
 const sendDailyPonto = async () => {
   const Pontos = new PontosService();
   const { pontos } = await Pontos.getData();
   const data = pontos.getRandom();
-  console.log(data);
   const message = Templates.DAILY(data);
   const { channel_id: destiny } = await connection('bot_tb_registered_channels')
     .select('channel_id')
@@ -19,10 +19,7 @@ const sendDailyPonto = async () => {
   console.log({ destiny });
   if (destiny) {
     if (data.hasAudio) {
-      const audio = await MessageMedia.fromUrl(
-        'https://rodcordeiro.github.io/shares/audio/marcha_imperial.mp3 ',
-      );
-      console.log('audio', audio);
+      const audio = await MessageMedia.fromUrl(data.audioLink);
       const msg = await client.sendMessage(destiny, message);
       await msg.reply('', undefined, {
         media: audio,
@@ -33,8 +30,16 @@ const sendDailyPonto = async () => {
   }
 };
 
-(() => {
-  const interval = setInterval(async () => {
-    await sendDailyPonto();
-  }, DAY_IN_MS);
-})();
+schedule(
+  DAILY_CRON,
+  async () => {
+    console.log('Running daily jobs');
+    sendDailyPonto();
+  },
+  {
+    name: 'daily',
+    runOnInit: true,
+    timezone: 'America/Sao_Paulo',
+    scheduled: true,
+  },
+);
