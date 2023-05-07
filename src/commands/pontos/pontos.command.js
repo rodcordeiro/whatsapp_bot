6c9f87fa7da2d066ed7250a9544284b9fbcdadc1
rodcordeiro <rodrigomendoncca@gmail.com>
2023-05-07 03:16:04 +0000

    Fix: pontos cpmmand return

--- src/commands/pontos/pontos.command.js
+++ src/commands/pontos/pontos.command.js
@@ -13,9 +13,9 @@ class Command {
     const { pontos} = await Data.getData()
     if(!args.length)
       return await message.reply("\tComando inválido!\nO formato correto é:\n\`\`\`/pontos id_linha\`\`\`\nPara vizualizar os ids das linhas, mande \`\`\`/categoria\`\`\`");
-    const da =pontos.data.find(item=>item.idLinha === args[0])
+    const da =pontos.data.filter(item=>item.idLinha === args[0])
     console.debug({da})
-   return await message.reply(JSON.JSON.stringify(da))
+   return await message.reply(JSON.stringify(da))
     
     } catch (error) {
       console.error(error)
